7fb624bfa807e4b42f7e6cd79fa9cd7ae8bef730
Cherise <nashpaiessy@gmail.com>
2023-09-21 13:09:00 +0300

    done

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -2,14 +2,14 @@
 import requests
 import json
 
-class GetPrograms:
-
-  def get_programs(self):
+def get_programs(self):
     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
     response = requests.get(URL)
     return response.content
 
+programs = GetPrograms()
+programs_schools = programs.program_school()
 
-programs = GetPrograms().get_programs()
-print(programs)
+for school in set(programs_schools):
+    print(school)
